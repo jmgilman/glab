c6efe9bbfb46192137b7197888f32c44bf65c30a
Joshua Gilman <joshuagilman@gmail.com>
2020-05-09 12:30:21 -0700

    Copies kube config to user directory and deletes unecessary shell script

--- ansible/env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Set vSphere variables
-export VMWARE_SERVER="vcenter.gilman.io"
-export VMWARE_USERNAME="$(vault kv get -field=username secret/vsphere/api)"
-export VMWARE_PASSWORD="$(vault kv get -field=password secret/vsphere/api)"
